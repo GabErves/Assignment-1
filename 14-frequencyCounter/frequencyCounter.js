337113d62fe2dcb439aac153d30f6b88b30e2c01
GabErves <Gabrielle.Erves@cix.csi.cuny.edu>
2022-01-03 14:30:12 -0500

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,20 @@
+ // Insert code here;
+  //create empty set
+  //loop word
+  //make variable to store characters
+  //if letter in set, increment, else ass
+//return object - done
+
 function frequencyCounter(word) {
-  // Insert code here;
+  var frequency = {};
+  for(var i =0; i<word.length; i++){
+    let letter = word.charAt(i); //charAt() gets a char at some index
+    if(frequency[letter]){
+      frequency[letter]++;
+    }else{
+      frequency[letter]=1;
+  }
+    return frequency;
 }
 
 // Do not edit this line;
